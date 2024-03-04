5aa2c82e35e17b8e96a47d2bbf43e202cfee2f88
rudolfthecreator <rudevichalex@mail.ru>
2024-03-04 17:00:37 +0300

    Создана базовая версия приложения 0.0.1.

---
+++ rudevichschool/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/App.css'
+import Version from './components/Version';
+
+function App() {
+  return (
+    <div className="App">
+      <Version/>
+    </div>
+  );
+}
+
+export default App;
---
+++ rudevichschool/src/components/Version.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Version = () => {
+  return <div>Базовая версия приложения 0.0.1</div>;
+};
+
+export default Version;
